635d169a3a7b365e0913bbb2caba905eb6f45f25
Jan Boerman <JanBoerman95@gmail.com>
2025-11-06 01:21:48 +0100

    Initial NewArrayTransformer setup.

--- agent/src/main/java/memorymonitoring/agent/NewArrayTransformer.java
+++ agent/src/main/java/memorymonitoring/agent/NewArrayTransformer.java
@@ -3,6 +3,14 @@
  */
 package memorymonitoring.agent;
 
+import java.lang.classfile.ClassFile;
+import java.lang.classfile.ClassModel;
+import java.lang.classfile.ClassTransform;
+import java.lang.classfile.CodeBuilder;
+import java.lang.classfile.CodeElement;
+import java.lang.classfile.instruction.NewMultiArrayInstruction;
+import java.lang.classfile.instruction.NewPrimitiveArrayInstruction;
+import java.lang.classfile.instruction.NewReferenceArrayInstruction;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
@@ -26,8 +34,31 @@ public final class NewArrayTransformer implements ClassFileTransformer {
             return null;
         }
 
-        // TODO
-        return classfileBuffer;
+        ClassFile classFile = ClassFile.of();
+        ClassModel classModel = classFile.parse(classfileBuffer);
+
+        return classFile.transformClass(classModel, ClassTransform.transformingMethodBodies((CodeBuilder codeBuilder, CodeElement codeElement) -> {
+
+            if (codeElement instanceof NewPrimitiveArrayInstruction newPrimitiveArrayInstruction) {
+                codeBuilder.with(codeElement);
+                // TODO grant write access to the whole array
+            }
+
+            else if (codeElement instanceof NewReferenceArrayInstruction newReferenceArrayInstruction) {
+                codeBuilder.with(codeElement);
+                // TODO grant write access to the whole array
+            }
+
+            else if (codeElement instanceof NewMultiArrayInstruction newMultiArrayInstruction) {
+                codeBuilder.with(codeElement);
+                // TODO grant write access to the whole array
+            }
+
+            else {
+                // Leave all other instructions unchanged.
+                codeBuilder.with(codeElement);
+            }
+        }));
     }
 
 }
